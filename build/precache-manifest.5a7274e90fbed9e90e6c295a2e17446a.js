d5d0ae74783be49269a9d237276201a93fbc4adf
thakurnitin2684 <thakurnitin2684@gmail.com>
2022-06-14 17:14:17 +0530

    title

---
+++ build/precache-manifest.5a7274e90fbed9e90e6c295a2e17446a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b42a336b58d9d78432becaa4dd35f6",
+    "url": "/react-notes/index.html"
+  },
+  {
+    "revision": "789df511536b62e5a45d",
+    "url": "/react-notes/static/js/2.5d894227.chunk.js"
+  },
+  {
+    "revision": "2f36d563ba029421375f",
+    "url": "/react-notes/static/js/main.1a5b5b35.chunk.js"
+  },
+  {
+    "revision": "569c68cdbacd6c1a5ff4",
+    "url": "/react-notes/static/js/runtime-main.67d3018d.js"
+  }
+]);
